fa4461ebf07841c37467cf86d3e7c5081f2c55b0
wengyusu <903459917@qq.com>
2015-12-06 22:07:36 +0800

    ver 1.1

---
+++ index_index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1",maximum-scale=1,user-scalable=no>
+	<title>签到主界面</title>
+	<link rel="stylesheet" type="text/css" href="index_index.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="head">
+			
+			<h1><span id="pen"></span>2016届毕业生秋季双选会<br/>签到系统</h1>
+		</div>
+		<div class="choose">
+			<div class="school">
+				<i></i><input type="text" style="width:80%" placeholder="请选择您的学校" readonly="readonly">
+			</div>
+			<div class="name">
+				<i></i><input type="text" style="width:80%" placeholder="请输入您的姓名" readonly="readonly">
+			</div>
+		</div>
+	</div>
+
+	<div class="bt">
+		<button id="bt1">签到</button>
+		<button id="bt2">代签</button>
+	</div>
+	<div class="success">
+		<h1>签到成功</h1>
+		<p><b>贵单位展位号是</b></p>
+		<p>欢迎来我校选拔人才</p>
+	</div>>
+	<script type="text/javascript" src="jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="index.js"></script>		
+</body>
+</html>
